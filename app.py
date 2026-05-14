5955638b761ca20f5215dc2f04a868486a71efbb
Demeshwar Rana <raghavranawat43@gmail.com>
2026-05-14 11:57:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ weekend_factor = np.where(hours > 120, 0.75, 1.0)
 
 df_trend = pd.DataFrame({
     'Hour': hours,
-    'PM25_Lag': (pollution_base + pollution_peaks)*(pollution_scale/60),
+    'PM25_Lag': (pollution_base + pollution_peaks)*pollution_scale,
     'Admissions': (35 + np.roll(pollution_peaks, 48) * 0.4)* (admission_scale/10)
 })
 
