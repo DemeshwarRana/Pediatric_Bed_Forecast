78c7ca96002c75375e3bb3a65dc923aa09a55ef6
Demeshwar Rana <raghavranawat43@gmail.com>
2026-05-14 11:36:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,8 +80,10 @@ st.subheader("48-Hour Ripple: How Air Quality Predicts Pediatric Bed Surges.")
 now = datetime.datetime.now()
 current_weekday = now.weekday()
 current_hour_of_week = (current_weekday * 24) + now.hour
-pollution_scale = pm25 / 60 
-admission_scale = ((pm25 + so2) / 60) + (1 - temp/40) + (1 - rh/100)
+pollution_scale = (pm25 + so2) / 60 
+admission_scale = pollution_scale + (1 - temp/45) + (1 - rh/100)
+df_trend['PM25_Lag'] = (pollution_base * pollution_scale) + pollution_peaks
+df_trend['Admissions'] = admission_base * admission_scale
 hours = np.arange(168)
 pollution_base = 60 + np.sin(hours / 4) * 10 + np.random.normal(0, 5, 168)
 pollution_peaks = np.array([30 if h in [20, 68, 116, 150] else 0 for h in hours])
